af042f326d0333ab404de4247de8e5a7059edb36
Viktor Daniel Varga <vvd007@gmail.com>
2022-12-28 09:05:59 +0100

    Some styling + about box

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,8 @@ import { ToolbarModule as PrimeNG_ToolbarModule } from 'primeng/toolbar';
 import { SplitButtonModule as PrimeNG_SplitButton } from 'primeng/splitbutton';
 import { CardModule as PrimeNG_CardModule } from 'primeng/card';
 import { TagModule as PrimeNG_TagModule } from 'primeng/tag';
+import { PanelModule as PrimeNG_PanelModule } from 'primeng/panel';
+import { DividerModule as PrimeNG_DividerModule } from 'primeng/divider';
 
 // CUSTOM COMPONENTS
 import { AppComponent } from './app.component';
@@ -39,6 +41,8 @@ import { ProjectListItemComponent } from './projects/project-list-item/project-l
     PrimeNG_SplitButton,
     PrimeNG_CardModule,
     PrimeNG_TagModule,
+    PrimeNG_PanelModule,
+    PrimeNG_DividerModule,
 
     RouterModule.forRoot([
       { path: '', component: HomeComponent },
